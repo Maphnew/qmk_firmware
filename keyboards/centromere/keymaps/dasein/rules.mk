a640f407874bf76ab8359e99744e04415a12c74e
sein park <seinpark@seins-Mac-mini.local>
2020-07-04 01:05:24 +0900

    set valid centromere work

--- keyboards/centromere/keymaps/dasein/rules.mk
+++ keyboards/centromere/keymaps/dasein/rules.mk
@@ -1,5 +1,7 @@
 # MCU name
-MCU = atmega32u2
+## default나 default_u2의 경우 이것을 atmega32u2로 갖고 있다. 그 결과 컴파일 해도 제대로 동작하지 않았다. 
+## 현재는 atmega32u4 chip인 모양이다.
+MCU = atmega32u4 
 
 # Bootloader selection
 #   Teensy       halfkay
